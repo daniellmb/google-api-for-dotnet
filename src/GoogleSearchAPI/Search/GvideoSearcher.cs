17d711bd779806b22e90bc6004f4e6780d1390d7
iron9light <iron9light@48684ff1-494f-0410-9814-895797430d20>
2008-06-18 08:59:18 +0000

    Add classes for Google Video Search. Add static method RFC2822DateTimeParse in SearchUtility.

---
+++ src/GoogleSearchAPI/Search/GvideoSearcher.cs
@@ -0,0 +1,84 @@
+﻿/**
+ * GvideoSearcher.cs
+ *
+ * Copyright (C) 2008,  iron9light
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+using System;
+using System.Net;
+
+namespace Google.API.Search
+{
+    public static class GvideoSearcher
+    {
+        private static int s_Timeout = 0;
+
+        /// <summary>
+        /// Get or set the length of time, in milliseconds, before the request times out.
+        /// </summary>
+        public static int Timeout
+        {
+            get
+            {
+                return s_Timeout;
+            }
+            set
+            {
+                if (s_Timeout < 0)
+                {
+                    throw new ArgumentOutOfRangeException("value");
+                }
+                s_Timeout = value;
+            }
+        }
+
+        internal static SearchData<GvideoResult> GSearch(string keyword, int start, ResultSizeEnum resultSize, SortType sortBy)
+        {
+            if (keyword == null)
+            {
+                throw new ArgumentNullException("keyword");
+            }
+
+            GvideoSearchRequest request = new GvideoSearchRequest(keyword, start, resultSize, sortBy);
+
+            WebRequest webRequest;
+            if (Timeout != 0)
+            {
+                webRequest = request.GetWebRequest(Timeout);
+            }
+            else
+            {
+                webRequest = request.GetWebRequest();
+            }
+
+            SearchData<GvideoResult> responseData;
+            try
+            {
+                responseData = RequestUtility.GetResponseData<SearchData<GvideoResult>>(webRequest);
+            }
+            catch (GoogleAPIException ex)
+            {
+                throw new SearchException(string.Format("request:\"{0}\"", request), ex);
+            }
+            return responseData;
+        }
+    }
+}
