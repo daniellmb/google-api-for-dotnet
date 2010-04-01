52a46bdd117d390f2a495f0fee16aca4937bfd46
iron9light <iron9light@48684ff1-494f-0410-9814-895797430d20>
2010-04-01 13:22:25 +0000

    Delete branch 0.4 since it is trunk now.

--- branches/0.4/build/runbuild.cmd
+++
@@ -1,2 +0,0 @@
-powershell -Command "& {Import-Module ..\tool\psake\psake.psm1; Invoke-psake .\build.ps1 %*}"
-pause
--- branches/0.4/build/runbuild.ps1
+++
@@ -1,5 +0,0 @@
-﻿cls
-
-Import-Module '..\tool\psake\psake.psm1'
-Invoke-psake '.\build.ps1' Test -framework 3.5
-Remove-Module psake
